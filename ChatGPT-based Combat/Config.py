dd0c3726dfd0528715cbefe415cd36934d7d1f0b
1 <1>
2023-08-24 22:07:29 +0800

    配置7Day版本英雄技能

--- ChatGPT-based Combat/Config.py
+++ ChatGPT-based Combat/Config.py
@@ -1,26 +1,6 @@
-from Skill import Skill
 from ExcelConfig import Hero
 
-Uni_attack = Skill(name='Attack', definition='normal', type='instant',
-                   effects_param=[{'effect_type': 'atk_dmg', 'dmg_coef': 1.0, 'target_area': 'single'}])
 
-Gem_skill = Skill(name='Gem_skill', definition='active', type='instant',
-                  effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 2.0, 'max_targets': 3,
-                                  'target_area': 'single'}])
-
-Nitro_skill = Skill(name='Nitro_skill', definition='active', type='instant',
-                    effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 5, 'target_area': 'single'},
-                                   {'effect_type': 'skill_dmg', 'dmg_coef': 5, 'cast_prob_lower_bound': 0.2,
-                                    'cast_prob_upper_bound': 0.2, 'target_area': 'single'}])
-
-Don_skill = Skill(name='Don_skill', definition='active', type='instant',
-                  effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 3.0, 'max_targets': 1,
-                                  'target_area': 'single'},
-                                 {'effect_type': 'buff', 'buffs': {'skill_dmg_rcv_inc': -0.5}, 'max_targets': 2,
-                                  'cast_prob_lower_bound': 0.3, 'cast_prob_upper_bound': 0.6, 'duration': 3,
-                                  'target_area': 'single', 'target_type': 'ally'}])
-
-# Massager = Skill(name='Massager_skill', definition='active', type='instant')
 
 def_coef = [60, 68, 76, 84, 92, 100, 108, 116, 124, 132, 140, 148, 156, 164, 172, 180, 188, 196, 204, 212, 224, 236,
             248, 260, 272, 284, 296, 308, 320, 332, 344, 356, 368, 380, 392, 404, 416, 428, 440, 452]
---
+++ ChatGPT-based Combat/HeroSkillConfig.py
@@ -0,0 +1,94 @@
+from Skill import Skill
+
+
+Uni_attack = Skill(name='普通攻击', definition='normal', type='instant',
+                   effects_param=[{'effect_type': 'atk_dmg', 'dmg_coef': 1.0, 'target_area': 'single'}])
+
+Gem_skill = Skill(name='杰玛技能', definition='active', type='instant',
+                  effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 2.0, 'max_targets': 3,
+                                  'target_area': 'single'}])
+
+Nitro_skill = Skill(name='奈乔技能', definition='active', type='instant',
+                    effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 5, 'target_area': 'single'},
+                                   {'effect_type': 'skill_dmg', 'dmg_coef': 5, 'cast_prob_lower_bound': 0.2,
+                                    'cast_prob_upper_bound': 0.2, 'target_area': 'single'}])
+
+Don_skill = Skill(name='东技能', definition='active', type='instant',
+                  effects_param=[{'effect_type': 'skill_dmg', 'dmg_coef': 3.0, 'max_targets': 1,
+                                  'target_area': 'single'},
+                                 {'effect_type': 'buff', 'buffs': {'skill_dmg_rcv_inc': -0.2}, 'max_targets': 2,
+                                  'cast_prob_lower_bound': 0.3, 'cast_prob_upper_bound': 0.6, 'duration': 3,
+                                  'target_area': 'single', 'target_type': 'ally'}])
+
+Massager_skill = Skill(name='按摩师技能', definition='active', type='instant',
+                       effects_param=[
+                           {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 1, 'target_area': 'single'},
+                           {'effect_type': 'buff', 'buffs': {'defense_multi': 0.2}, 'max_targets': 1,
+                            'target_type': 'self'}, ])
+
+GoldenNose_skill = Skill(name='金鼻子技能', definition='active', type='instant',
+                         effects_param=[
+                             {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 1, 'target_area': 'single'},
+                             {'effect_type': 'buff', 'buffs': {'rage_increase_add': 5}, 'max_targets': 2,
+                              'target_type': 'ally'}, ])
+
+Susie_skill = Skill(name='苏茜技能', definition='active', type='instant',
+                    effects_param=[
+                        {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 2, 'target_area': 'single'},
+                        {'effect_type': 'buff', 'buffs': {'atk_dmg_deal_inc': -0.2}, 'max_targets': 2,
+                            'target_type': 'enemy'}, ])
+
+FemaleDoctor_skill = Skill(name='女医生技能', definition='active', type='instant',
+                            effects_param=[
+                                {'effect_type': 'heal', 'dmg_coef': 1.0, 'max_targets': 2, 'target_area': 'single',
+                                    'target_type': 'ally'}, ])
+
+Artist_skill = Skill(name='艺术家技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 2, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'continuous_atk': 1}, 'max_targets': 1,
+                                'target_type': 'self'}, ])
+
+Judge_skill = Skill(name='审判长技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 3, 'target_area': 'single'}])
+
+Jaden_skill = Skill(name='杰登技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 2, 'target_area': 'multiple'}])
+
+Singer_skill = Skill(name='歌手技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 3, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'dmg_rcv_inc': 0.2}, 'max_targets': 3,
+                                'target_type': 'ally'}, ])
+
+Gilbert_skill = Skill(name='吉尔伯特技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 3, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'chase_atk': 1}, 'max_targets': 1,
+                                'target_type': 'self'}, ])
+
+Egirl_skill = Skill(name='E-girl技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 1, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'atk_multi': 0.2}, 'max_targets': 1,
+                                'target_type': 'self'}, ])
+
+Watt_skill = Skill(name='华特技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 2, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'reverse_atk': 1}, 'max_targets': 1,
+                                'target_type': 'self'}, ])
+
+Strongman_skill = Skill(name='硬汉技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 3, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'teammate_guard': 0.2}, 'max_targets': 2,
+                                'target_type': 'ally'}, ])
+
+Nancy_skill = Skill(name='南希技能', definition='active', type='instant',
+                        effects_param=[
+                            {'effect_type': 'skill_dmg', 'dmg_coef': 1.0, 'max_targets': 3, 'target_area': 'single'},
+                            {'effect_type': 'buff', 'buffs': {'atk_dmg_deal_inc': 0.2}, 'max_targets': 3, 'duration': 3,
+                                'target_type': 'enemy'}, ])
